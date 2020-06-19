aa41d2e8fbfd904b59d06c75c8dd63ae24f49754
Thierry Chappuis <thierry@chappuis.io>
2020-06-19 17:30:00 +0200

    Added color support to the example

--- exemple.py
+++ exemple.py
@@ -2,17 +2,31 @@ import curses
 
 
 def center(screen, lines):
+    """Generator centering the lines at the center of the screen."""
     height, width = screen.getmaxyx()
     y = (height - len(lines)) // 2
-
     for i, line in enumerate(lines):
         x = (width - len(line)) // 2
         yield y + i, x, line
 
 
-def main(screen):
+def clear(screen):
+    """clear the screen with the chosen default background color."""
     screen.clear()
+    height, width = screen.getmaxyx()
+    for y in range(height - 1):
+        screen.addstr(y, 0, " " * width, curses.color_pair(1))
+
+
+def main(screen):
+    """Main entry point of the example."""
+    # setting cursor visibility
     curses.curs_set(False)
+
+    # setting color pairs and clearing the screen with the choosen colors
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_WHITE)
+    clear(screen)
 
     lines = [
         "Petit exemple de code avec curses",
@@ -22,8 +36,10 @@ def main(screen):
     ]
 
     for y, x, line in center(screen, lines):
-        screen.addstr(y, x, line)
+        screen.addstr(y, x, line, curses.color_pair(1))
 
+    screen.refresh()
+    # waiting the user press a key to quit the example
     key = screen.getch()
 
 
